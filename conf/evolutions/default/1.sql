9eab3fecb83e36a50c088df5fa7f9dfae1851a98
Shakhal Levinson <shakhalevinson@gmail.com>
2017-10-16 03:38:13 +0200

    pg

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,25 +2,30 @@
 
 # --- !Ups
 
+CREATE SEQUENCE bookmark_seq;
+
 CREATE TABLE bookmark (
-  id                        BIGINT NOT NULL AUTO_INCREMENT,
+  id                        BIGINT NOT NULL DEFAULT NEXTVAL ('bookmark_seq'),
   name                      VARCHAR(255) NOT NULL,
   url                       VARCHAR(1000) NOT NULL,
   slug                      VARCHAR(255) NOT NULL,
   userId                    BIGINT NOT NULL,
   CONSTRAINT pk_bookmark PRIMARY KEY (id),
-  CONSTRAINT uq_userid_url UNIQUE (userId,url),
-  INDEX (slug),
-  INDEX (url)
-)
-;
-
-CREATE TABLE user (
-  userId                    BIGINT NOT NULL AUTO_INCREMENT,
-  email                     VARCHAR(255) NOT NULL,
-  CONSTRAINT pk_user PRIMARY KEY (userId),
-  INDEX (email)
+  CONSTRAINT uq_userid_url UNIQUE (userId,url)
 );
+
+CREATE INDEX on slug_idx bookmark(slug);
+CREATE INDEX on url_idx bookmark(url);
+
+CREATE SEQUENCE user_seq;
+
+CREATE TABLE users (
+  userId                    BIGINT NOT NULL DEFAULT NEXTVAL ('user_seq'),
+  email                     VARCHAR(255) NOT NULL,
+  CONSTRAINT pk_user PRIMARY KEY (userId)
+);
+
+CREATE INDEX email_idx on users(email);
 
 CREATE TABLE token (
   userId                    BIGINT NOT NULL,
@@ -29,8 +34,9 @@ CREATE TABLE token (
   CONSTRAINT pk_token PRIMARY KEY (token)
 )
 ;
+
 # --- !Downs
 
 drop table if exists bookmark;
-drop table if exists user;
+drop table if exists users;
 drop table if exists token;
